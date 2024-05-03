efd2989ff10becd03683c9af8554caacce698b41
jhon954 <jhon.moreno451@gmail.com>
2024-05-03 11:36:27 -0500

    Create a new branch to continue the job and create a css file to tasks unassigned

--- htdocs/page_plastInnova/admin/tasks_admin_unassigned.php
+++ htdocs/page_plastInnova/admin/tasks_admin_unassigned.php
@@ -14,6 +14,7 @@
     <!-- styles -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link href="../css/styles_nav_bar.css" rel="stylesheet">
+    <link href="../css/styles_unassigned_tasks.css" rel="stylesheet">
     <!-- script -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
